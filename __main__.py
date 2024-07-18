c613ad788541957f1e5ed6aac518236966473131
spike <2411123479@qq.com>
2024-07-18 15:41:01 +0800

    增加允许访问目录

--- __main__.py
+++ __main__.py
@@ -524,6 +524,7 @@ class ChatBot(LeftElem, ChatbotElem, RightElem, Settings, Config, FakeComponents
         # 过滤掉不允许用户访问的路径
         self.demo.blocked_paths = get_files_and_dirs(
             path=init_path.base_path, filter_allow=['users_private', 'gpt_log', 'docs'])
+        self.demo.allowed_paths = ['users_private', 'gpt_log', 'docs']
         login_html = '登陆即注册，请记住你自己的账号和密码'
         self.demo.auth_message = login_html
         if AUTHENTICATION == 'SQL':
